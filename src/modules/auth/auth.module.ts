6d6d01b1d664cc8eb80ffd87796af1498647d32a
liangxinwei <sivan.shanghai@gmail.com>
2021-09-23 23:24:20 +0800

    update: code reivew

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from 'src/guards';
 import { JwtStrategy } from 'src/strategies';
 
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 
 @Module({
   imports: [
--- src/strategies/JwtStrategy.ts
+++ src/strategies/JwtStrategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from 'src/modules/auth/constants';
+import { jwtConstants } from 'src/guards';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
